abe081ada9d239cc5fac1734296d216e014bec2d
MattMrak <matthewmrakovcic@gmail.com>
2021-03-22 16:49:52 -0400

    Edit Feedback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -22,6 +22,19 @@ class FeedbacksController < ApplicationController
         @feedback = Feedback.find_by(id: params[:id])
     end
 
+    def edit
+        @feedback = Feedback.find_by(id: params[:id])
+    end
+
+    def update
+        @feedback = Feedback.find_by(id: params[:id])
+        if @feedback.update(feedback_params)
+          redirect_to feedback_path(@feedback)
+        else
+          render :edit
+        end
+    end
+
     private
     def feedback_params
         params.require(:feedback).permit(:notes,:mission_id)

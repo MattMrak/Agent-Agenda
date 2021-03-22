77e47c9a1774345c25f0360b96d2306eb450c724
MattMrak <matthewmrakovcic@gmail.com>
2021-03-22 16:40:58 -0400

    New Feedback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -8,5 +8,19 @@ class FeedbacksController < ApplicationController
     def new
         @feedback = Feedback.new
     end
-    
+
+    def create
+        @feedback = current_user.feedbacks.build(feedback_params)
+        if @feedback.save
+            redirect_to feedbacks_path
+        else
+            render :new
+        end
+    end
+
+    private
+    def feedback_params
+        params.require(:feedback).permit(:notes,:mission_id)
+    end
+
 end

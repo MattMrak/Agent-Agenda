ed709bb54553f44244f8e8a93e1f332edaab5fed
MattMrak <matthewmrakovcic@gmail.com>
2021-03-23 17:55:58 -0400

    Most Feedback Scope Method

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -5,6 +5,7 @@ class Mission < ApplicationRecord
   has_many :users, through: :feedbacks
 
   scope :alpha, -> { order(:operation) }
+  scope :most_feedbacks, -> { joins(:feedbacks).group('missions.id').order('count(missions.id) desc') }
 
   def category_attributes=(attr)
     self.category = Category.find_or_create_by(attr) if !attr[:urgency_level].blank?

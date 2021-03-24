1cbc93af688cf9ffc238d9d74386fff024754286
MattMrak <matthewmrakovcic@gmail.com>
2021-03-24 13:00:34 -0400

    New Feedback Uniqueness w/ Error

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,4 +2,6 @@ class Feedback < ApplicationRecord
     belongs_to :user
     belongs_to :mission
     validates :notes, presence: true
+    validates :notes, uniqueness: {scope: :user_id, message: "already exists"}
+
 end
